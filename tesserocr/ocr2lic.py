eb6c91874d8f412bd4515b1d81039e590903f947
Hamid Ebadi <hamid.ebadi@gmail.com>
2021-12-24 13:34:28 +0100

    rescale the letters

--- tesserocr/ocr2lic.py
+++ tesserocr/ocr2lic.py
@@ -3,6 +3,22 @@ import pytesseract
 import numpy as np
 filename = 'sample3.jpg'
 
+def get_optimal_font_scale_height(text, height):
+	for scale in reversed(range(0, 60, 1)):
+		textSize = cv2.getTextSize(text, fontFace=cv2.FONT_HERSHEY_DUPLEX, fontScale=scale/10, thickness=1)
+		new_height = textSize[0][1]
+		if (new_height <= height-10):
+			return scale/10
+	return 1
+
+def get_optimal_font_scale_width(text, width):
+	for scale in reversed(range(0, 60, 1)):
+		textSize = cv2.getTextSize(text, fontFace=cv2.FONT_HERSHEY_DUPLEX, fontScale=scale/10, thickness=1)
+		new_width = textSize[0][0]
+		if (new_width <= width):
+			return scale/10
+	return 1
+
 # read the image and get the dimensions
 img = cv2.imread(filename)
 h, w, d = img.shape # assumes color image
@@ -30,13 +46,18 @@ for b in boxes.splitlines():
 	font = cv2.FONT_HERSHEY_SIMPLEX
 	# org
 	org = (x1, y1)
-	# fontScale
-	fontScale = 0.5
+
+	scale_y = get_optimal_font_scale_height(letter, y2-y1)
+	scale_x = get_optimal_font_scale_width(letter, x2-x1)
+
+	fontScale = min(scale_x, scale_y)
+
 	# Black
 	color = (0, 0, 0)
 	# Line thickness of 2 px
-	thickness = 1
+	thickness = 2
 	# Using cv2.putText() method
+
 	blank_image = cv2.putText(blank_image, letter, org, font, fontScale, color, thickness, cv2.LINE_AA)
 
 # show annotated image and wait for keypress
